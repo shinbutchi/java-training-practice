737b91a3f65d6ae97cd8140292cb3b0b1b190643
shin <shinbutchi@ymail.com>
2015-04-15 01:43:04 +0700

    Create MVC project #1

---
+++ spring-mvc-practice/src/main/java/vn/smartdev/javatrainingpractice/springmvcpractice/entities/Role.java
@@ -0,0 +1,52 @@
+package vn.smartdev.javatrainingpractice.springmvcpractice.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name="role")
+public class Role  extends AbstractAuditableEntity implements Serializable {
+    private String id;
+    private String roleName;
+
+    @Id
+    @Column(name = "id", nullable = false, insertable = true, updatable = true, length = 45)
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "role_name", nullable = true, insertable = true, updatable = true, length = 255)
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role = (Role) o;
+
+        if (id != null ? !id.equals(role.id) : role.id != null) return false;
+        if (roleName != null ? !roleName.equals(role.roleName) : role.roleName != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (roleName != null ? roleName.hashCode() : 0);
+        return result;
+    }
+}
---
+++ spring-mvc-practice/src/main/java/vn/smartdev/javatrainingpractice/springmvcpractice/entities/UserRole.java
@@ -0,0 +1,58 @@
+package vn.smartdev.javatrainingpractice.springmvcpractice.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_role", schema = "", catalog = "java_training")
+public class UserRole {
+    private String id;
+    private Role roleByRoleId;
+    private User userByUserId;
+
+    @Id
+    @Column(name = "id", nullable = false, insertable = true, updatable = true, length = 45)
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserRole userRole = (UserRole) o;
+
+        if (id != null ? !id.equals(userRole.id) : userRole.id != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "role_id", referencedColumnName = "id")
+    public Role getRoleByRoleId() {
+        return roleByRoleId;
+    }
+
+    public void setRoleByRoleId(Role roleByRoleId) {
+        this.roleByRoleId = roleByRoleId;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    public User getUserByUserId() {
+        return userByUserId;
+    }
+
+    public void setUserByUserId(User userByUserId) {
+        this.userByUserId = userByUserId;
+    }
+}
