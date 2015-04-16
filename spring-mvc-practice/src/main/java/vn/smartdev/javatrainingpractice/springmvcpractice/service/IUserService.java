e546dca6c86ee50184afe856f2fe4b5da3a2feaf
shin <shinbutchi@ymail.com>
2015-04-17 02:01:12 +0700

    Create MVC project #1

--- spring-mvc-practice/src/main/java/vn/smartdev/javatrainingpractice/springmvcpractice/service/IUserService.java
+++ spring-mvc-practice/src/main/java/vn/smartdev/javatrainingpractice/springmvcpractice/service/IUserService.java
@@ -1,10 +1,8 @@
 package vn.smartdev.javatrainingpractice.springmvcpractice.service;
 
-import org.springframework.stereotype.Component;
 import vn.smartdev.javatrainingpractice.springmvcpractice.dto.UserDTO;
 import vn.smartdev.javatrainingpractice.springmvcpractice.entities.User;
 
-@Component
 public interface IUserService {
     User add(UserDTO userDTO);
 
